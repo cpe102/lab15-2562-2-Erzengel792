1dfae56761f6ec24d32fecfa0c13ea2c101d2cb1
Erzengel792 <tanadol_boat@hotmail.com>
2020-02-28 16:32:58 +0700

    Edited V.1

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -20,8 +20,47 @@ int main(){
 	
 	cout << "---------------------------------------------\n";
 	
-	findRowSum(dPtr,result,N,M);
+	findRowSum(dPtr,result,N,M); 
 	showData(result,N,1);
 }
 
-//Write definition of randData(), showData() and findRowSum()
+void randData(double *dPtr,int N , int M){
+	for(double i = 0;i<49 ; i++){
+			*dPtr = rand()%100;
+			*dPtr/=100;
+
+			dPtr+=1;
+	}
+}
+void showData(double *data , int N , int M){
+	for(int i = 0 ; i<N ; i++){
+		for(int j = 0 ; j<M;j++){
+			cout<< *data <<" ";
+			if(*data == 0.9 || *data == 0.8 || *data == 0.7 || *data == 0.6 || *data == 0.5 || *data == 0.4 || *data == 0.3 || *data == 0.2 || *data == 0.1 )cout<<" ";
+			else if(*data == 0)cout<< "   ";
+
+			data+=1;
+		}
+		cout<<"\n";
+	}
+}
+
+
+
+void findRowSum(const double *dPtr,double *result,int N,int M){
+	double sum = 0;
+	int count = 0;
+	for(double i = 0;i<49 ; i++){
+		sum+= *dPtr;
+		dPtr+=1;
+		count++;
+		if(count == 7){
+			*result = sum;
+			sum = 0;
+			result+=1;
+			count = 0;
+		}
+		   
+	}
+
+}
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -24,4 +24,41 @@ int main(){
 	showData(result,1,M);
 }
 
-//Write definition of randData(), showData() and findColSum()
+void randData(double *dPtr,int N , int M){
+	for(double i = 0;i<49 ; i++){
+			*dPtr = rand()%100;
+			*dPtr/=100;
+
+			dPtr+=1;
+	}
+}
+void showData(double *data , int N , int M){
+	for(int i = 0 ; i<N ; i++){
+		for(int j = 0 ; j<M;j++){
+			cout<< *data <<" ";
+			if(*data == 0.9 || *data == 0.8 || *data == 0.7 || *data == 0.6 || *data == 0.5 || *data == 0.4 || *data == 0.3 || *data == 0.2 || *data == 0.1 )cout<<" ";
+			else if(*data == 0)cout<< "   ";
+
+			data+=1;
+		}
+		cout<<"\n";
+	}
+}
+
+void findColSum(const double *dPtr,double *result,int N,int M){
+	double sum = 0;
+	int count = 0;
+	for(double i = 0;i<49 ; i++){
+		sum+= *dPtr;
+		dPtr+=7;
+		count++;
+		if(count > 6){
+			*result = sum;
+			sum = 0;
+			result+=1;
+			dPtr-=7*count;
+			count = 0;
+			dPtr+=1;
+		}
+	}
+}
